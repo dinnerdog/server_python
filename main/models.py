c4b993b83cde40a182543f634f4bb805529f5461
Ma Le <58284515+dinnerdog@users.noreply.github.com>
2024-03-04 13:40:58 +1300

    Add files via upload

---
+++ main/models.py
@@ -0,0 +1,266 @@
+#coding:utf-8
+__author__ = "ila"
+from django.db import models
+
+from .model import BaseModel
+
+from datetime import datetime
+
+
+
+class xuesheng(BaseModel):
+    __doc__ = u'''xuesheng'''
+    __tablename__ = 'xuesheng'
+
+    __loginUser__='xuehao'
+
+
+    __authTables__={}
+    __authPeople__='是'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __loginUserColumn__='xuehao'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='否'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    xuehao=models.CharField ( max_length=255,null=False,unique=True, verbose_name='学号' )
+    mima=models.CharField ( max_length=255,null=False, unique=False, verbose_name='密码' )
+    xingming=models.CharField ( max_length=255,null=False, unique=False, verbose_name='姓名' )
+    nianling=models.IntegerField  (  null=True, unique=False, verbose_name='年龄' )
+    xingbie=models.CharField ( max_length=255, null=True, unique=False, verbose_name='性别' )
+    shouji=models.CharField ( max_length=255, null=True, unique=False, verbose_name='手机' )
+    zhaopian=models.CharField ( max_length=255, null=True, unique=False, verbose_name='照片' )
+    '''
+    xuehao=VARCHAR
+    mima=VARCHAR
+    xingming=VARCHAR
+    nianling=Integer
+    xingbie=VARCHAR
+    shouji=VARCHAR
+    zhaopian=VARCHAR
+    '''
+    class Meta:
+        db_table = 'xuesheng'
+        verbose_name = verbose_name_plural = '学生'
+class jiaoshi(BaseModel):
+    __doc__ = u'''jiaoshi'''
+    __tablename__ = 'jiaoshi'
+
+    __loginUser__='jiaoshizhanghao'
+
+
+    __authTables__={}
+    __authPeople__='是'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __loginUserColumn__='jiaoshizhanghao'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='否'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    jiaoshizhanghao=models.CharField ( max_length=255,null=False,unique=True, verbose_name='教师账号' )
+    mima=models.CharField ( max_length=255,null=False, unique=False, verbose_name='密码' )
+    jiaoshixingming=models.CharField ( max_length=255,null=False, unique=False, verbose_name='教师姓名' )
+    nianling=models.IntegerField  (  null=True, unique=False, verbose_name='年龄' )
+    xingbie=models.CharField ( max_length=255, null=True, unique=False, verbose_name='性别' )
+    shouji=models.CharField ( max_length=255, null=True, unique=False, verbose_name='手机' )
+    zhaopian=models.CharField ( max_length=255, null=True, unique=False, verbose_name='照片' )
+    '''
+    jiaoshizhanghao=VARCHAR
+    mima=VARCHAR
+    jiaoshixingming=VARCHAR
+    nianling=Integer
+    xingbie=VARCHAR
+    shouji=VARCHAR
+    zhaopian=VARCHAR
+    '''
+    class Meta:
+        db_table = 'jiaoshi'
+        verbose_name = verbose_name_plural = '教师'
+class kechengxinxi(BaseModel):
+    __doc__ = u'''kechengxinxi'''
+    __tablename__ = 'kechengxinxi'
+
+
+
+    __authTables__={'jiaoshizhanghao':'jiaoshi',}
+    __authPeople__='否'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='是'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='是'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='是'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='是'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    kechengmingcheng=models.CharField ( max_length=255, null=True, unique=False, verbose_name='课程名称' )
+    kechengleixing=models.CharField ( max_length=255, null=True, unique=False, verbose_name='课程类型' )
+    kaikeshijian=models.DateTimeField  (  null=True, unique=False, verbose_name='开课时间' )
+    jieshushijian=models.DateTimeField  (  null=True, unique=False, verbose_name='结束时间' )
+    shangkexuesheng=models.TextField   (  null=True, unique=False, verbose_name='上课学生' )
+    kechengneirong=models.TextField   (  null=True, unique=False, verbose_name='课程内容' )
+    fengmian=models.CharField ( max_length=255, null=True, unique=False, verbose_name='封面' )
+    jiaoshizhanghao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='教师账号' )
+    jiaoshixingming=models.CharField ( max_length=255, null=True, unique=False, verbose_name='教师姓名' )
+    thumbsupnum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='赞' )
+    crazilynum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='踩' )
+    clicktime=models.DateTimeField  (  null=True, unique=False, verbose_name='最近点击时间' )
+    clicknum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='点击次数' )
+    '''
+    kechengmingcheng=VARCHAR
+    kechengleixing=VARCHAR
+    kaikeshijian=DateTime
+    jieshushijian=DateTime
+    shangkexuesheng=Text
+    kechengneirong=Text
+    fengmian=VARCHAR
+    jiaoshizhanghao=VARCHAR
+    jiaoshixingming=VARCHAR
+    thumbsupnum=Integer
+    crazilynum=Integer
+    clicktime=DateTime
+    clicknum=Integer
+    '''
+    class Meta:
+        db_table = 'kechengxinxi'
+        verbose_name = verbose_name_plural = '课程信息'
+class kechengyuyue(BaseModel):
+    __doc__ = u'''kechengyuyue'''
+    __tablename__ = 'kechengyuyue'
+
+
+
+    __authTables__={'jiaoshizhanghao':'jiaoshi','xuehao':'xuesheng',}
+    __authPeople__='否'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='是'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='是'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    kechengmingcheng=models.CharField ( max_length=255, null=True, unique=False, verbose_name='课程名称' )
+    yuyueshijian=models.DateTimeField  (  null=True, unique=False, verbose_name='预约时间' )
+    yuyueshizhang=models.CharField ( max_length=255, null=True, unique=False, verbose_name='预约时长' )
+    yuyueyuanyin=models.TextField   ( null=False, unique=False, verbose_name='预约原因' )
+    jiaoshizhanghao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='教师账号' )
+    jiaoshixingming=models.CharField ( max_length=255, null=True, unique=False, verbose_name='教师姓名' )
+    xuehao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='学号' )
+    xingming=models.CharField ( max_length=255, null=True, unique=False, verbose_name='姓名' )
+    sfsh=models.CharField ( max_length=255, null=True, unique=False,default='否', verbose_name='是否审核' )
+    shhf=models.TextField   (  null=True, unique=False, verbose_name='审核回复' )
+    '''
+    kechengmingcheng=VARCHAR
+    yuyueshijian=DateTime
+    yuyueshizhang=VARCHAR
+    yuyueyuanyin=Text
+    jiaoshizhanghao=VARCHAR
+    jiaoshixingming=VARCHAR
+    xuehao=VARCHAR
+    xingming=VARCHAR
+    sfsh=VARCHAR
+    shhf=Text
+    '''
+    class Meta:
+        db_table = 'kechengyuyue'
+        verbose_name = verbose_name_plural = '课程预约'
+class quxiaoyuyue(BaseModel):
+    __doc__ = u'''quxiaoyuyue'''
+    __tablename__ = 'quxiaoyuyue'
+
+
+
+    __authTables__={'jiaoshizhanghao':'jiaoshi','xuehao':'xuesheng',}
+    __authPeople__='否'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='是'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='是'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    kechengmingcheng=models.CharField ( max_length=255, null=True, unique=False, verbose_name='课程名称' )
+    quxiaoshijian=models.DateTimeField  (  null=True, unique=False, verbose_name='取消时间' )
+    quxiaoyuanyin=models.TextField   ( null=False, unique=False, verbose_name='取消原因' )
+    jiaoshizhanghao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='教师账号' )
+    jiaoshixingming=models.CharField ( max_length=255, null=True, unique=False, verbose_name='教师姓名' )
+    xuehao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='学号' )
+    xingming=models.CharField ( max_length=255, null=True, unique=False, verbose_name='姓名' )
+    sfsh=models.CharField ( max_length=255, null=True, unique=False,default='否', verbose_name='是否审核' )
+    shhf=models.TextField   (  null=True, unique=False, verbose_name='审核回复' )
+    '''
+    kechengmingcheng=VARCHAR
+    quxiaoshijian=DateTime
+    quxiaoyuanyin=Text
+    jiaoshizhanghao=VARCHAR
+    jiaoshixingming=VARCHAR
+    xuehao=VARCHAR
+    xingming=VARCHAR
+    sfsh=VARCHAR
+    shhf=Text
+    '''
+    class Meta:
+        db_table = 'quxiaoyuyue'
+        verbose_name = verbose_name_plural = '取消预约'
+class storeup(BaseModel):
+    __doc__ = u'''storeup'''
+    __tablename__ = 'storeup'
+
+
+
+    __authTables__={}
+    __authSeparate__='是'#后台列表权限
+    __foreEndListAuth__='是'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    userid=models.BigIntegerField  ( null=False, unique=False, verbose_name='用户id' )
+    refid=models.BigIntegerField  (  null=True, unique=False, verbose_name='收藏id' )
+    tablename=models.CharField ( max_length=255, null=True, unique=False, verbose_name='表名' )
+    name=models.CharField ( max_length=255,null=False, unique=False, verbose_name='收藏名称' )
+    picture=models.CharField ( max_length=255,null=False, unique=False, verbose_name='收藏图片' )
+    type=models.CharField ( max_length=255, null=True, unique=False,default='1', verbose_name='类型(1:收藏,21:赞,22:踩)' )
+    inteltype=models.CharField ( max_length=255, null=True, unique=False, verbose_name='推荐类型' )
+    '''
+    userid=BigInteger
+    refid=BigInteger
+    tablename=VARCHAR
+    name=VARCHAR
+    picture=VARCHAR
+    type=VARCHAR
+    inteltype=VARCHAR
+    '''
+    class Meta:
+        db_table = 'storeup'
+        verbose_name = verbose_name_plural = '收藏表'
+class news(BaseModel):
+    __doc__ = u'''news'''
+    __tablename__ = 'news'
+
+
+
+    __authTables__={}
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    title=models.CharField ( max_length=255,null=False, unique=False, verbose_name='标题' )
+    introduction=models.TextField   (  null=True, unique=False, verbose_name='简介' )
+    picture=models.CharField ( max_length=255,null=False, unique=False, verbose_name='图片' )
+    content=models.TextField   ( null=False, unique=False, verbose_name='内容' )
+    '''
+    title=VARCHAR
+    introduction=Text
+    picture=VARCHAR
+    content=Text
+    '''
+    class Meta:
+        db_table = 'news'
+        verbose_name = verbose_name_plural = '通知公告'
